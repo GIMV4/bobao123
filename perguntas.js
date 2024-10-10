bf16ca9e54083abe9df05e349c05f5b20b71ca58
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-10-10 11:50:08 -0300

    dsadas

---
+++ perguntas.js
@@ -0,0 +1,81 @@
+criaCartao(
+    
+    "Geografia",
+    "Qual e a montanha mais alta do mundo",
+    "Monte Evereste")
+    criaCartao(
+    "Historia",
+    "Qual foi a guerra que ocorreu no Paraná e Santa Catarina?",
+    "Guerra do contestado")
+    criaCartao(
+    "Geografia",
+    "Qual a capital da Itália?",
+    "Roma")
+    criaCartao(
+    "Quimica",
+    "Qual e o significado de P na tabela periodica",
+    "Fosforo")
+    criaCartao(
+    "Biologia",
+    "Qual a representaçao dos alelos recessivos",
+    "aa")
+    criaCartao(
+    "Geografia",
+    "Qual a capital de Paraná",
+    "Curitiba")
+    criaCartao(
+    "Geografia",
+    "Qual a capital de Santa Catarina",
+    "Florianoploles")
+    criaCartao(
+    "Geografia",
+    "Qual a capital da Inglaterra",
+    "Londres")
+    criaCartao(
+    "Geografia",
+    "Qual a capital de Brasil",
+    "Brasilia")
+    criaCartao(
+    "Geografia",
+    "Qual a capital de Minas Gerais",
+    "Belo Horizonte")
+    criaCartao(
+    "Educaçao Financeira",
+    "Quanto e 25% de R$100,00",
+    "R$25,00")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de O da tabela periodica",
+    "Oxigênio")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de Na da tabela periodica",
+    "Sodio")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de Mg da tabela periodica",
+    "Magnesio")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de Fe da tabela periodica",
+    "Ferro")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de U da tabela periodica",
+    "Uranio")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de H da tabela periodica",
+    "Hidrogenio")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de Cf da tabela periodica",
+    "Carlifornio")
+    criaCartao(
+    "Quimica",
+    "Qual e sgnificado de S da tabela periodica",
+    "Enxofre")
+    criaCartao(
+    'Matematica',
+    'Qual e a tangente de 45°',
+    '1')
