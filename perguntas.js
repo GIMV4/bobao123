54369b4d2c1ab43f7e884a39794ecc00bba24114
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-10-10 11:52:58 -0300

    das

--- perguntas.js
+++ perguntas.js
@@ -1,80 +1,80 @@
 criaCartao(
     
-    "Geografia",
-    "Qual e a montanha mais alta do mundo",
-    "Monte Evereste")
+    'Geografia',
+    'Qual e a montanha mais alta do mundo',
+    'Monte Evereste')
     criaCartao(
-    "Historia",
-    "Qual foi a guerra que ocorreu no Paraná e Santa Catarina?",
-    "Guerra do contestado")
+    'Historia',
+    'Qual foi a guerra que ocorreu no Paraná e Santa Catarina?',
+    'Guerra do contestado')
     criaCartao(
-    "Geografia",
-    "Qual a capital da Itália?",
-    "Roma")
+    'Geografia',
+    'Qual a capital da Itália?',
+    'Roma')
     criaCartao(
-    "Quimica",
-    "Qual e o significado de P na tabela periodica",
-    "Fosforo")
+    'Quimica',
+    'Qual e o significado de P na tabela periodica',
+    'Fosforo')
     criaCartao(
-    "Biologia",
-    "Qual a representaçao dos alelos recessivos",
-    "aa")
+    'Biologia',
+    'Qual a representaçao dos alelos recessivos',
+    'aa')
     criaCartao(
-    "Geografia",
-    "Qual a capital de Paraná",
-    "Curitiba")
+    'Geografia',
+    'Qual a capital de Paraná',
+    'Curitiba')
     criaCartao(
-    "Geografia",
-    "Qual a capital de Santa Catarina",
-    "Florianoploles")
+    'Geografia',
+    'Qual a capital de Santa Catarina',
+    'Florianoploles')
     criaCartao(
-    "Geografia",
-    "Qual a capital da Inglaterra",
-    "Londres")
+    'Geografia',
+    'Qual a capital da Inglaterra',
+    'Londres')
     criaCartao(
-    "Geografia",
-    "Qual a capital de Brasil",
-    "Brasilia")
+    'Geografia',
+    'Qual a capital de Brasil',
+    'Brasilia')
     criaCartao(
-    "Geografia",
-    "Qual a capital de Minas Gerais",
-    "Belo Horizonte")
+    'Geografia',
+    'Qual a capital de Minas Gerais',
+    'Belo Horizonte')
     criaCartao(
-    "Educaçao Financeira",
-    "Quanto e 25% de R$100,00",
-    "R$25,00")
+    'Educaçao Financeira',
+    'Quanto e 25% de R$100,00',
+    'R$25,00')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de O da tabela periodica",
-    "Oxigênio")
+    'Quimica',
+    'Qual e sgnificado de O da tabela periodica',
+    'Oxigênio')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de Na da tabela periodica",
-    "Sodio")
+    'Quimica',
+    'Qual e sgnificado de Na da tabela periodica',
+    'Sodio')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de Mg da tabela periodica",
-    "Magnesio")
+    'Quimica',
+    'Qual e sgnificado de Mg da tabela periodica',
+    'Magnesio')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de Fe da tabela periodica",
-    "Ferro")
+    'Quimica',
+    'Qual e sgnificado de Fe da tabela periodica',
+    'Ferro')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de U da tabela periodica",
-    "Uranio")
+    'Quimica',
+    'Qual e sgnificado de U da tabela periodica',
+    'Uranio')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de H da tabela periodica",
-    "Hidrogenio")
+    'Quimica',
+    'Qual e sgnificado de H da tabela periodica',
+    'Hidrogenio')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de Cf da tabela periodica",
-    "Carlifornio")
+    'Quimica',
+    'Qual e sgnificado de Cf da tabela periodica',
+    'Carlifornio')
     criaCartao(
-    "Quimica",
-    "Qual e sgnificado de S da tabela periodica",
-    "Enxofre")
+    'Quimica',
+    'Qual e sgnificado de S da tabela periodica',
+    'Enxofre')
     criaCartao(
     'Matematica',
     'Qual e a tangente de 45°',
