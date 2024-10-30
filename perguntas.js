128ef61b8f918ef723de3af225f4cc59918a067a
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-10-30 07:54:33 -0300

    dfghjkl

--- perguntas.js
+++ perguntas.js
@@ -1,81 +1,80 @@
 criaCartao(
-    
     'Geografia',
     'Qual e a montanha mais alta do mundo',
     'Monte Evereste')
-    criaCartao(
+criaCartao(
     'Historia',
     'Qual foi a guerra que ocorreu no Paraná e Santa Catarina?',
     'Guerra do contestado')
-    criaCartao(
+criaCartao(
     'Geografia',
     'Qual a capital da Itália?',
     'Roma')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e o significado de P na tabela periodica',
     'Fosforo')
-    criaCartao(
+criaCartao(
     'Biologia',
     'Qual a representaçao dos alelos recessivos',
     'aa')
-    criaCartao(
+criaCartao(
     'Geografia',
     'Qual a capital de Paraná',
     'Curitiba')
-    criaCartao(
+criaCartao(
     'Geografia',
     'Qual a capital de Santa Catarina',
     'Florianoploles')
-    criaCartao(
+criaCartao(
     'Geografia',
     'Qual a capital da Inglaterra',
     'Londres')
-    criaCartao(
+criaCartao(
     'Geografia',
     'Qual a capital de Brasil',
     'Brasilia')
-    criaCartao(
+criaCartao(
     'Geografia',
     'Qual a capital de Minas Gerais',
     'Belo Horizonte')
-    criaCartao(
+criaCartao(
     'Educaçao Financeira',
     'Quanto e 25% de R$100,00',
     'R$25,00')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de O da tabela periodica',
     'Oxigênio')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de Na da tabela periodica',
     'Sodio')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de Mg da tabela periodica',
     'Magnesio')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de Fe da tabela periodica',
     'Ferro')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de U da tabela periodica',
     'Uranio')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de H da tabela periodica',
     'Hidrogenio')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de Cf da tabela periodica',
     'Carlifornio')
-    criaCartao(
+criaCartao(
     'Quimica',
     'Qual e sgnificado de S da tabela periodica',
     'Enxofre')
-    criaCartao(
+criaCartao(
     'Matematica',
     'Qual e a tangente de 45°',
     '1')
